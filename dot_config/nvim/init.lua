d67c4d8ba74c7d8059458e5324825dafb2de8cdc
iwa <yuki@giamnote.net>
2022-11-29 13:59:27 +0900

    feat: update indent-blankline config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -90,6 +90,10 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
--- vim.g.indent_blankline_space_char = '·'
-vim.g.indent_blankline_use_treesitter = true
-vim.g.indent_blankline_show_end_of_line = true
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+require'indent_blankline'.setup {
+  space_char_blankline = " ",
+  show_current_context = true,
+  show_current_context_start = true,
+}
