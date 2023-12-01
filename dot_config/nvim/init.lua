77809c98802a23b85140959c41fa526567013914
iwa <yuki@giamnote.net>
2023-12-01 14:44:19 +0900

    feat: update golang settings

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,6 +5,7 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.cmdheight = 0
 vim.opt.expandtab = true
 vim.opt.laststatus = 3
+vim.opt.listchars = "extends:…,precedes:…,tab:»-,nbsp:␣"
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.spelllang = "en,cjk"
@@ -12,6 +13,13 @@ vim.opt.spelllang = "en,cjk"
 vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
   pattern = {"go.sum", "go.work.sum"},
   callback = function() vim.opt.filetype = "gosum" end
+})
+vim.api.nvim_create_autocmd({"FileType"}, {
+  pattern = {"go"},
+  callback = function()
+    vim.opt.expandtab = false
+    vim.opt.shiftwidth = 0
+  end
 })
 
 require("config.lazy")
