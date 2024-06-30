4f85b1b2732d2a9fdceb40a414770ba65edb709d
iwa <yuki@giamnote.net>
2024-06-30 21:25:35 +0900

    feat(nvim): Migrate to LazyVim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,25 +1,2 @@
-vim.g.mapleader = " "
-vim.g.python3_host_prog = "~/.asdf/shims/python3"
-
-vim.opt.clipboard = "unnamedplus"
-vim.opt.cmdheight = 0
-vim.opt.expandtab = true
-vim.opt.laststatus = 3
-vim.opt.listchars = "extends:…,precedes:…,tab:»-,nbsp:␣"
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.spelllang = "en,cjk"
-
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-  pattern = {"go.sum", "go.work.sum"},
-  callback = function() vim.opt.filetype = "gosum" end
-})
-vim.api.nvim_create_autocmd({"FileType"}, {
-  pattern = {"go"},
-  callback = function()
-    vim.opt.expandtab = false
-    vim.opt.shiftwidth = 0
-  end
-})
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
