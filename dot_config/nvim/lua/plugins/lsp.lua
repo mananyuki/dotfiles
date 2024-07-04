8c22b279d24e5abcf777d3f111e2c9176c42a1a8
iwa <yuki@giamnote.net>
2024-07-04 15:39:48 +0900

    feat(lsp): Update lsp config

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
       opts.sources = vim.list_extend(opts.sources or {}, {
         nls.builtins.diagnostics.commitlint,
         nls.builtins.diagnostics.textlint,
-        nls.builtins.formatting.biome,
+        nls.builtins.formatting.biome.with({ extra_args = { "--indent-style", "space" } }),
         nls.builtins.formatting.buf,
         require("none-ls.formatting.rustfmt"),
         require("none-ls.formatting.trim_newlines"),
